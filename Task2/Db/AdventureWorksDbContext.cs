02867157284adda9b46567620edb8c11c2a13ba7
Setanta <miasango2@hotmail.com>
2022-05-12 22:26:59 -0600

    Tarea1

---
+++ Task2/Db/AdventureWorksDbContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Task2.Db
+{
+    public partial class AdventureWorksDbContext : DbContext
+    {
+        public AdventureWorksDbContext()
+        {
+        }
+
+        public AdventureWorksDbContext(DbContextOptions<AdventureWorksDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<ProductCategory> ProductCategories { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=tareaapi.database.windows.net;Initial Catalog=Adventure;Persist Security Info=True;User ID=AdminUser;Password=Pass1234");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductCategory>(entity =>
+            {
+                entity.ToTable("ProductCategory");
+
+                entity.Property(e => e.ProductCategoryId).HasColumnName("ProductCategoryID");
+
+                entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Rowguid)
+                    .HasColumnName("rowguid")
+                    .HasDefaultValueSql("(newid())");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Task2/Db/ProductCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2.Db
+{
+    public partial class ProductCategory
+    {
+        public int ProductCategoryId { get; set; }
+        public string Name { get; set; } = null!;
+        public Guid Rowguid { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
